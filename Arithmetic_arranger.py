6b2a61ef3afe2413bf4d2a26473cdb1fff8d10b0
Almaz Vafin <almaz.vafin@bimeister.com>
2025-01-15 17:41:43 +0300

    step1 finished

--- Arithmetic_arranger.py
+++ Arithmetic_arranger.py
@@ -1,6 +1,23 @@
 def arithmetic_arranger(problems, show_answers=False):
-
+    mainlist=[] #список для слогаемых
+    maxlenlist=[] # список максимальных длин строк слогаемых
+    for i in problems:
+        item1= i.split()
+        mainlist.append(item1)
+        lenlist = []
+        for i in item1:
+            len1=len(i)
+            lenlist.append(len1)
+        maxlenlist.append(max(lenlist))
+    #print(maxlenlist)
+    #print(mainlist)
+    i=0
+    for i in range(len(mainlist)):
+        newstring=mainlist[i][0].rjust(maxlenlist[i]+2)+'    '+ mainlist[i+1][0].rjust(maxlenlist[i+1]+2)
+    print(newstring)
     return problems
 
-print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
+arithmetic_arranger(["32 + 698","3801 - 2"])
 
+#print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
+#print(f'\n{arithmetic_arranger(["32 + 698"])}')
